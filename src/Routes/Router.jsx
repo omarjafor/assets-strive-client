c14dcd166fad79233bf3b0fdf3965e70c47bdbc3
omarjafor <omarjaforchy@gmail.com>
2023-11-26 09:48:50 +0600

    admin and user route define

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,10 +4,14 @@ import Home from "../Pages/Home/Home";
 import JoinEmployee from "../Pages/JoinEmployee/JoinEmployee";
 import JoinAdmin from "../Pages/JoinAdmin/JoinAdmin";
 import Login from "../Pages/Login/Login";
-import UserHome from "../Pages/UserHome/UserHome";
-import AdminHome from "../Pages/AdminHome/AdminHome";
+import UserHome from "../Pages/User/UserHome";
+import AdminHome from "../Pages/Admin/AdminHome";
 import AdminLayout from "../Layout/adminlayout";
 import UserLayout from "../Layout/UserLayout";
+import MyAssets from "../Pages/User/MyAssets/MyAssets";
+import MyTeam from "../Pages/User/MyTeam/MyTeam";
+import RequestAsset from "../Pages/User/RequestAsset/RequestAsset";
+import CustomRequest from "../Pages/User/CustomRequest/CustomRequest";
 
 const router = createBrowserRouter([
     {
@@ -37,6 +41,22 @@ const router = createBrowserRouter([
             {
                 path:'home',
                 element: <UserHome></UserHome>
+            },
+            {
+                path: 'myassets',
+                element: <MyAssets></MyAssets>
+            },
+            {
+                path: 'myteam',
+                element: <MyTeam></MyTeam>
+            },
+            {
+                path: 'requestasset',
+                element: <RequestAsset></RequestAsset>
+            },
+            {
+                path: 'customrequest',
+                element: <CustomRequest></CustomRequest>
             }
         ]
     },
