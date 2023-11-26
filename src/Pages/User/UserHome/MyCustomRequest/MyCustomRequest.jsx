ed404926bc371f87ed55ba9812cc6d57e1529e62
omarjafor <omarjaforchy@gmail.com>
2023-11-26 18:35:28 +0600

    user home custom request section

--- src/Pages/User/UserHome/MyCustomRequest/MyCustomRequest.jsx
+++ src/Pages/User/UserHome/MyCustomRequest/MyCustomRequest.jsx
@@ -1,14 +1,79 @@
+import useAuth from "../../../../Hooks/useAuth";
 import useCustomRequest from "../../../../Hooks/useCustomRequest";
+import RequestCard from "./RequestCard";
 
 
 const MyCustomRequest = () => {
-
-    const { customRequest, isLoading } = useCustomRequest();
+    const { user } = useAuth();
+    const { customRequest, isLoading, refetch } = useCustomRequest();
     console.log(customRequest, isLoading);
 
+    if (isLoading) return 
+    <div className="mx-auto items-center text-center">
+        <div className="animate-pulse block-item shadow-md max-w-xl mx-auto mt-20">
+            <div className="bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 h-6 rounded-t-3xl"></div>
+            <div className="py-4 px-6">
+                <div className="flex items-center space-x-2">
+                    <div className="h-7 w-7 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full"></div>
+                    <div className="h-3 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full w-1/3"></div>
+                </div>
+                <div className="my-6">
+                    <div className="h-5 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full w-3/4"></div>
+                    <div className="my-4">
+                        <div className="h-3 my-2 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full w-full"></div>
+                        <div className="h-3 my-2 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full w-5/6"></div>
+                        <div className="h-3 my-2 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full w-4/6"></div>
+                        <div className="h-3 my-2 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full w-5/6"></div>
+                        <div className="h-3 my-2 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full w-3/6"></div>
+                        <div className="h-3 my-2 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full w-2/6"></div>
+                    </div>
+                </div>
+                <div className="my-4">
+                    <div className="h-11 bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-lg w-full"></div>
+                    <div className="h-3 my-4 mx-auto bg-gradient-to-t from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full w-1/2"></div>
+                </div>
+            </div>
+        </div>
+    </div>;
+
     return (
-        <div>
-            My Custom Request Section
+        <div className="mx-12">
+            <div className="text-left my-4 items-center">
+                <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                    {user?.displayName}&apos;s Custom Request Section
+                </h1>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Asset Name</th>
+                            <th>Price</th>
+                            <th>Status</th>
+                            <th>Type</th>
+                            <th>View</th>
+                        </tr>
+                    </thead>
+                        {
+                            customRequest.map((items, index) => <RequestCard
+                            items={items}
+                            index={index}
+                            key={index}
+                                refetch={refetch}
+                            ></RequestCard>)
+                        }
+                </table>
+            </div>
+            {/* <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10 mx-auto justify-items-center'>
+                {
+                    customRequest.map(items => <RequestCard
+                        items={items}
+                    key={items._id}
+                    ></RequestCard>)
+                }
+            </div> */}
         </div>
     );
 };
--- src/Pages/User/UserHome/UserHome.jsx
+++ src/Pages/User/UserHome/UserHome.jsx
@@ -1,10 +1,21 @@
+import useAuth from "../../../Hooks/useAuth";
 import MyCustomRequest from "./MyCustomRequest/MyCustomRequest";
 
 
 const UserHome = () => {
+    const {user} = useAuth();
+
     return (
         <div>
-            User Home Heading.................
+            <div className="text-center my-4 items-center mx-auto">
+                <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                    Welcome Back, {user?.displayName} !
+                </h1>
+
+                <p className="mt-1.5 text-sm text-gray-500">
+                    Let&apos;s explore your assets here! 🎉
+                </p>
+            </div>
             <MyCustomRequest></MyCustomRequest>
         </div>
     );
