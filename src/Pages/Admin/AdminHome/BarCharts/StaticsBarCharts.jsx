e9134da15d44784a7653361b0900e309246e1663
omarjafor <omarjaforchy@gmail.com>
2023-11-30 21:36:27 +0600

    statistice service bar chart some updated

--- src/Pages/Admin/AdminHome/BarCharts/StaticsBarCharts.jsx
+++ src/Pages/Admin/AdminHome/BarCharts/StaticsBarCharts.jsx
@@ -1,21 +1,6 @@
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Rectangle } from 'recharts';
 import useAxiosSecure from '../../../../Hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
-
-const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
-const getPath = (x, y, width, height) => {
-    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
-  ${x + width / 2}, ${y}
-  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
-  Z`;
-};
-
-const TriangleBar = (props) => {
-    const { fill, x, y, width, height } = props;
-
-    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-};
 
 const StaticsBarCharts = () => {
     const axiosSecure = useAxiosSecure();
@@ -56,7 +41,7 @@ const StaticsBarCharts = () => {
                 height={300}
                 data={data}
                 margin={{
-                    top: 20,
+                    top: 5,
                     right: 30,
                     left: 20,
                     bottom: 5,
@@ -65,13 +50,10 @@ const StaticsBarCharts = () => {
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis />
-                <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-                    {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                    ))}
-                </Bar>
+                <Tooltip />
+                <Bar dataKey="pv" fill="#0088FE" activeBar={<Rectangle fill="#00C49F" stroke="blue" />} />
+                <Bar dataKey="uv" fill="#00C49F" activeBar={<Rectangle fill="#0088FE" stroke="purple" />} />
             </BarChart>
-            
         </div>
     );
 };
