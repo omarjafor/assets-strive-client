0954c5d852d969e69bda2914a8baf5cf25550cf3
omarjafor <omarjaforchy@gmail.com>
2023-12-03 18:36:37 +0600

    packages section design updated

--- src/Pages/Home/Packages/Packages.jsx
+++ src/Pages/Home/Packages/Packages.jsx
@@ -4,15 +4,21 @@ import { Link } from "react-router-dom";
 const Packages = () => {
 
     return (
-        <div>
+        <div className="mx-40 my-10">
             <div>
-                <h2 className="mb-6 block text-4xl font-bold leading-[1.3] tracking-normal text-[#0B0B0B] antialiased">
-                    Asset Strive Pricing Plans
-                </h2>
+                <div className="text-left mb-4 items-center mx-auto">
+                    <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                        Asset Strive Pricing
+                    </h1>
+
+                    <p className="mt-1.5 text-lg text-gray-500">
+                        As one of the world&apos;s leading assets managers, help you manage employee 🎉
+                    </p>
+                </div>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-3 justify-center justify-items-center gap-8 my-10">
                 <div className="relative flex w-full max-w-[20rem] flex-col rounded-xl bg-gradient-to-r from-teal-400 via-cyan-500 to-cyan-900 dark:from-gray-500 dark:via-gray-900 dark:to-black hover:bg-gradient-to-tl bg-clip-border p-8 text-white shadow-md shadow-pink-500/40">
-                    <div className="relative m-0 mb-8 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center text-gray-700 shadow-none">
+                    <div className="relative m-0 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-2 text-center text-gray-700 shadow-none">
                         <p className="block text-lg  font-bold uppercase leading-normal text-white antialiased">
                             standard
                         </p>
@@ -133,31 +139,9 @@ const Packages = () => {
                                     Basic reporting functionalities
                                 </p>
                             </li>
-                            <li className="flex items-center gap-4">
-                                <span className="rounded-full border border-white/20 bg-white/20 p-1">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth="2"
-                                        stroke="currentColor"
-                                        aria-hidden="true"
-                                        className="h-3 w-3"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4.5 12.75l6 6 9-13.5"
-                                        ></path>
-                                    </svg>
-                                </span>
-                                <p className="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                                    Email support 24/7
-                                </p>
-                            </li>
                         </ul>
                     </div>
-                    <div className="mt-12 p-0">
+                    <div className="mt-4 p-0">
                         <Link to='/joinadmin'
                             className="block w-full select-none rounded-lg bg-gradient-to-l from-teal-400 via-cyan-500 to-cyan-900 dark:from-gray-500 dark:via-gray-900 dark:to-black py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-black dark:text-white shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                             type="button"
@@ -168,7 +152,7 @@ const Packages = () => {
                     </div>
                 </div>
                 <div className="relative flex w-full max-w-[20rem] flex-col rounded-xl bg-gradient-to-r from-teal-400 via-cyan-500 to-cyan-900 dark:from-gray-500 dark:via-gray-900 dark:to-black hover:bg-gradient-to-tl bg-clip-border p-8 text-white shadow-md shadow-pink-500/40">
-                    <div className="relative m-0 mb-8 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center text-gray-700 shadow-none">
+                    <div className="relative m-0 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center text-gray-700 shadow-none">
                         <p className="block text-lg  font-bold uppercase leading-normal text-white antialiased">
                             Professional
                         </p>
@@ -289,31 +273,9 @@ const Packages = () => {
                                     Improved reporting features
                                 </p>
                             </li>
-                            <li className="flex items-center gap-4">
-                                <span className="rounded-full border border-white/20 bg-white/20 p-1">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth="2"
-                                        stroke="currentColor"
-                                        aria-hidden="true"
-                                        className="h-3 w-3"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4.5 12.75l6 6 9-13.5"
-                                        ></path>
-                                    </svg>
-                                </span>
-                                <p className="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                                    Email and phone support.
-                                </p>
-                            </li>
                         </ul>
                     </div>
-                    <div className="mt-12 p-0">
+                    <div className="mt-4 p-0">
                         <Link to='/joinadmin'
                             className="block w-full select-none rounded-lg bg-gradient-to-l from-teal-400 via-cyan-500 to-cyan-900 dark:from-gray-500 dark:via-gray-900 dark:to-black py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-black dark:text-white shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                             type="button"
@@ -324,7 +286,7 @@ const Packages = () => {
                     </div>
                 </div>
                 <div className="relative flex w-full max-w-[20rem] flex-col rounded-xl bg-gradient-to-r from-teal-400 via-cyan-500 to-cyan-900 dark:from-gray-500 dark:via-gray-900 dark:to-black hover:bg-gradient-to-tl bg-clip-border p-8 text-white shadow-md shadow-pink-500/40">
-                    <div className="relative m-0 mb-8 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center text-gray-700 shadow-none">
+                    <div className="relative m-0 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center text-gray-700 shadow-none">
                         <p className="block text-lg  font-bold uppercase leading-normal text-white antialiased">
                             Enterprise
                         </p>
@@ -445,31 +407,9 @@ const Packages = () => {
                                     Detailed reporting analytics
                                 </p>
                             </li>
-                            <li className="flex items-center gap-4">
-                                <span className="rounded-full border border-white/20 bg-white/20 p-1">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth="2"
-                                        stroke="currentColor"
-                                        aria-hidden="true"
-                                        className="h-3 w-3"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4.5 12.75l6 6 9-13.5"
-                                        ></path>
-                                    </svg>
-                                </span>
-                                <p className="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                                    Dedicated customer support.
-                                </p>
-                            </li>
                         </ul>
                     </div>
-                    <div className="mt-12 p-0">
+                    <div className="mt-4 p-0">
                         <Link to='/joinadmin'
                             className="block w-full select-none rounded-lg bg-gradient-to-l from-teal-400 via-cyan-500 to-cyan-900 dark:from-gray-500 dark:via-gray-900 dark:to-black py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-black dark:text-white shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                             type="button"
