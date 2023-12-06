c494451cdfae388ea00d108c1dfb73e6b43f99d9
omarjafor <omarjaforchy@gmail.com>
2023-12-06 18:19:54 +0600

    image url corupted issue fixed

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -41,7 +41,7 @@ const AdminRoute = ({ children }) => {
     if (user && isAdmin) {
         return children;
     }
-    return <Navigate to="/" state={{ from: location }} replace></Navigate>
+    return <Navigate to="/joinadmin" state={{ from: location }} replace></Navigate>
 };
 
 export default AdminRoute;
