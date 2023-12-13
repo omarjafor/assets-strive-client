dcadcae4e0bde7bd0e1e3247183d035cf7e769ba
omarjafor <omarjaforchy@gmail.com>
2023-12-13 18:21:54 +0600

    home page footer implemented

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import About from "./About/About";
 import Banner from "./Banner/Banner";
+import Footer from "./Footer/Footer";
 import Packages from "./Packages/Packages";
 import { Helmet } from 'react-helmet-async';
 
@@ -12,6 +13,7 @@ const Home = () => {
             <Banner></Banner>
             <About></About>
             <Packages></Packages>
+            <Footer></Footer>
         </div>
     );
 };
